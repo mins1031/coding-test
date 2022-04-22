cc8a5213e86839e64b99d93264e17a31fa78ea79
mins1031 <urisegea@naver.com>
2022-04-23 00:00:09 +0900

    p

---
+++ programmers/124NationNum.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = ''
+    while n:
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
+
+n = 15
+print(solution(n))
+"""
+내ㅔ가 푼거
+"""
