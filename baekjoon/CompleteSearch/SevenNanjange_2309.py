f0761884fd49fb5728cf453015c018223aa9d6a7
mins1031 <urisegea@naver.com>
2023-01-06 22:52:51 +0900

    코테풀기

---
+++ baekjoon/CompleteSearch/SevenNanjange_2309.py
@@ -0,0 +1,13 @@
+import sys
+
+result_sum = 100
+nums = []
+nanjange_count = 9
+
+for i in range(nanjange_count):
+    nums.append(input())
+
+sum_heights = sum(nums)
+
+for i in range(nanjange_count):
+
