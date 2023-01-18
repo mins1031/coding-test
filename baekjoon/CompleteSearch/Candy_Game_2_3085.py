d17372a5bdabd059a0946d13078b11ac4d3bfbe9
mins1031 <urisegea@naver.com>
2023-01-19 00:43:01 +0900

    브루트포s

--- baekjoon/CompleteSearch/Candy_Game_2_3085.py
+++ baekjoon/CompleteSearch/Candy_Game_2_3085.py
@@ -1,50 +1,61 @@
 import sys
 
+def check(arr, x, y) :
+    count = len(arr)
+    biggest = 1
+    temp_cnt = 1
+
+    for i in range(1, count) :
+        if arr[i][y] == arr[i-1][y] :
+            temp_cnt += 1
+        else:
+            temp_cnt = 1
+
+    if biggest < temp_cnt:
+        biggest = temp_cnt
+
+    temp_cnt = 1
+    for i in range(1, count):
+        if arr[x][i] == arr[x][i - 1]:
+            temp_cnt += 1
+        else:
+            temp_cnt = 1
+
+    if biggest < temp_cnt :
+        biggest = temp_cnt
+
+    return biggest
+
 n = int(sys.stdin.readline())
 board = []
-directions = [[0, -1], [0, 1], [-1, 0], [1, 0]] # 상하좌우
-results = []
-
-def swap(exist_x_position, exist_y_position, exist_color) :
-    for dir in directions:
-        target_x_position = exist_x_position + dir[0]
-        target_y_position = exist_y_position + dir[1]
-        if target_x_position < 0 or target_x_position >= n :
-            continue
-        elif target_y_position < 0 or target_y_position >= n :
-            continue
-
-        if board[target_x_position][target_y_position] == exist_color :
-            continue
-
-        board[exist_x_position][exist_y_position], board[target_x_position][target_y_position] = board[target_x_position][target_y_position], board[exist_x_position][exist_y_position]
-
-
-def move(exist_x, exist_y, target_x, target_y) :
-    overlaps = []
-    if exist_y == target_y and exist_x != target_x :
-        overlap_count = 0
-        for i in range(1, n):
-            if board[exist_x][i-1] == board[exist_x][i]:
-                overlap_count += 1
-            else:
-                overlaps.append(overlap_count)
-                overlap_count = 0
-
-    elif exist_y != target_y and exist_x == target_x :
-
-
-
+result = 0
 
 for i in range(n):
     one_row = input()
     board.append(list(one_row))
 
-
-
 for i in range(n):
     for j in range(n):
-        board[i][j]
+        temp = 0
+        if j+1 < n:
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+            temp = check(board, i, j)
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+
+        if temp > result :
+            result = temp
+
+        temp = 0
+
+        if i+1 < n :
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+            temp = check(board, i, j)
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+
+        if temp > result :
+            result = temp
+
+print(result)
 
 
 
